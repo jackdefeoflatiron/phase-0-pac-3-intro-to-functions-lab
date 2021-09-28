0e23eeedfd219ea953dbce1f11888efbe55d01f2
John DeFeo <jackd3243@gmail.com>
2021-09-28 18:49:07 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout() {
+    console.log('HELLO')
+}
+function logWhisper() {
+    console.log('hello')
+}
+function sayHiToGrandma(string) {
+    if (string.toLowerCase("hello"))
+    return ( "I can't hear you!");
+    else if (string.toUpperCase("hello"))
+    return ("YES INDEED!")
+    else if (string === "I love you, Grandma.")
+    return ("I love you, too.")
+
+}
